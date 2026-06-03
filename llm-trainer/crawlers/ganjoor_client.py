1c38fa6c1fb33b2d9efa527f486be702d1516853
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-03 21:45:23 +0330

    create : ganjoor client

---
+++ llm-trainer/crawlers/ganjoor_client.py
@@ -0,0 +1,17 @@
+from crawlers.http_client import HttpClient
+
+class GanjoorClient:
+    BASE_URL = "https://api.ganjoor.net/api"
+    
+    def __init__(self):
+        self.http = HttpClient()
+        
+    def get_poets(self):
+        return self.http.get(
+            f"{self.BASE_URL}/ganjoor/poets"
+        )
+        
+    def get_category(self,category_id):
+        return self.http.get(
+            f"{self.BASE_URL}/ganjoor/cat/{category_id}?poems=true&mainSections=false"
+        )
