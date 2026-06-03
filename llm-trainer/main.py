3aa387b08411b2aa2a6685090f361a0c10d96354
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-03 23:26:21 +0330

    Update main.py

--- llm-trainer/main.py
+++ llm-trainer/main.py
@@ -2,8 +2,12 @@ import sys
 
 from crawlers.ganjoor import GanjoorCrawler
 from crawlers.ganjoor_client import GanjoorClient
+
 from storage.raw_storage import RawStorage
 from storage.state_storage import StateStorage
+
+from processors.poem_processor import PoemProcessor
+from dataset.exporter import DatasetExporter
 
 
 def build_crawler():
@@ -14,21 +18,58 @@ def build_crawler():
     )
 
 
+def build_processor():
+    return PoemProcessor(
+        RawStorage()
+    )
+
+
+def build_exporter():
+    return DatasetExporter(    )
+
+
 def main():
+
     if len(sys.argv) < 2:
-        print("Usage: python3 main.py <command>")
-        print("Commands: crawl_all")
+        print(
+            """
+Commands:
+
+crawl_all
+process
+export
+pipeline
+"""
+        )
         return
 
     command = sys.argv[1]
 
-    crawler = build_crawler()
-
     if command == "crawl_all":
+
+        crawler = build_crawler()
         crawler.crawl_all()
 
-    elif command == "crawl":
-        crawler.crawl()
+    elif command == "process":
+
+        processor = build_processor()
+        processor.process_all()
+
+    elif command == "export":
+
+        exporter = build_exporter()
+        exporter.export()
+
+    elif command == "pipeline":
+
+        crawler = build_crawler()
+        crawler.crawl_all()
+
+        processor = build_processor()
+        processor.process_all()
+
+        exporter = build_exporter()
+        exporter.export()
 
     else:
         print(f"Unknown command: {command}")
