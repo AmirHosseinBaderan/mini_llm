156b84d34b4955345846a35982b586ae761eae90
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-03 21:37:14 +0330

    update : make http client better

--- llm-trainer/crawlers/http_client.py
+++ llm-trainer/crawlers/http_client.py
@@ -1,12 +1,49 @@
+import time
 import requests
 
+from storage.cache import FileCache
+
+
 class HttpClient:
-    
-    def get(self,url):
-        response = requests.get(
-            url,
-            timeout=50
-        )
-        
-        response.raise_for_status()
-        return response.json()
+
+    def __init__(self):
+
+        self.cache = FileCache()
+
+    def get(self, url):
+
+        cached = self.cache.get(url)
+
+        if cached:
+            return cached
+
+        retries = 3
+
+        for attempt in range(retries):
+
+            try:
+
+                response = requests.get(
+                    url,
+                    timeout=30
+                )
+
+                response.raise_for_status()
+
+                data = response.json()
+
+                self.cache.set(
+                    url,
+                    data
+                )
+
+                time.sleep(0.5)
+
+                return data
+
+            except Exception:
+
+                if attempt == retries - 1:
+                    raise
+
+                time.sleep(2)
